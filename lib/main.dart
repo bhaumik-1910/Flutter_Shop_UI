700a4148fa35f07ca360c53d871dd924ed26eee5
bhaumik <bhaumikkothiya1@gmail.com>
2024-12-12 08:47:14 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:shop_ui/screen/welcome_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.black,
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
